bc05f0967fef07cc3e2427487a013c48720702cb
庞博 <bopang@sohu-inc.com>
2015-03-24 14:28:28 +0800

    Init.
    Signed-off-by: 庞博 <bopang@sohu-inc.com>

---
+++ NavigationTest/ViewController.m
@@ -0,0 +1,26 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+
+    self.navigationController.navigationBarHidden = NO;
+}
+
+@end
---
+++ NavigationTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NavigationTest
+//
+//  Created by waterforest on 15-3-24.
+//  Copyright (c) 2015年 Sohu.com Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
